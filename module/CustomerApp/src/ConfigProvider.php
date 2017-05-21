3761e5c191031e132c4e35e945a6820a4287ca47
Ralf Eggert <ralf@travello.com>
2017-05-21 21:40:38 +0200

    Module angelegt und konfiguriert

---
+++ module/CustomerApp/src/ConfigProvider.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * IPC2017 Zend\Expressive Workshop
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/RalfEggert/ipc2017-expressive
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace CustomerApp;
+
+/**
+ * Class ConfigProvider
+ *
+ * @package CustomerApp
+ */
+class ConfigProvider
+{
+    /**
+     * @return array
+     */
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+            'templates'    => $this->getTemplates(),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function getTemplates(): array
+    {
+        return [];
+    }
+}
---
+++ module/CustomerDomain/src/ConfigProvider.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * IPC2017 Zend\Expressive Workshop
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/RalfEggert/ipc2017-expressive
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace CustomerDomain;
+
+/**
+ * Class ConfigProvider
+ *
+ * @package CustomerDomain
+ */
+class ConfigProvider
+{
+    /**
+     * @return array
+     */
+    public function __invoke() : array
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getDependencies() : array
+    {
+        return [];
+    }
+}
